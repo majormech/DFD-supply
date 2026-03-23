6229c5a27a33e410c6ef4517f7f1c1e7310d37a9
majormech <62382005+majormech@users.noreply.github.com>
2026-03-23 17:45:50 -0500

    Initialize project configuration in index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+name = "dfd-supply"
+main = "src/index.js"
+compatibility_date = "2026-03-23"
+
+[assets]
+directory = "./public"
+binding = "ASSETS"
+
+[[d1_databases]]
+binding = "DB"
+database_name = "dfd-supply"
+database_id = "replace-with-your-d1-database-id"
+migrations_dir = "migrations"
